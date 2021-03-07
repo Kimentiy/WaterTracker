d20c58c4fe6b1d1c0e487b55c357dd8759ed8bb4
Kirill <kmalafey@gmail.com>
2021-03-08 01:02:26 +0300

    Added simple test file uploading

--- app/src/main/java/com/kimentiy/watertracker/MainActivity.kt
+++ app/src/main/java/com/kimentiy/watertracker/MainActivity.kt
@@ -11,7 +11,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build()
+    private val client = DbxClientV2(config, BuildConfig.API_KEY)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,18 +24,34 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button).setOnClickListener {
             showAccountInfo()
         }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            uploadFile()
+        }
     }
 
     private fun showAccountInfo() {
         GlobalScope.launch {
             withContext(Dispatchers.IO) {
-                val config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build()
-                val client = DbxClientV2(config, BuildConfig.API_KEY)
-
                 val account = client.users().currentAccount
 
                 withContext(Dispatchers.Main) {
                     findViewById<TextView>(R.id.text).text = account.name.displayName
+                }
+            }
+        }
+    }
+
+    private fun uploadFile() {
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                assets.open("simplefile").use { inputStream ->
+                    val metadata =
+                        client.files().uploadBuilder("/test.txt").uploadAndFinish(inputStream)
+
+                    withContext(Dispatchers.Main) {
+                        findViewById<TextView>(R.id.text).text =
+                            "File uploaded, ${metadata.pathDisplay}"
+                    }
                 }
             }
         }
